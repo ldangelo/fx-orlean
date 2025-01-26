c534385093cfd1cc9400880121cc48420d807b4c
Leo D'Angelo <ldangelo@mac.com>
2025-01-26 06:58:56 -0600

    Have Duende.BFF integrated with backend

--- src/EventServer/EventServer/Program.cs
+++ src/EventServer/EventServer/Program.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using EventServer;
 using EventServer.Client.Services;
 using EventServer.Components;
@@ -31,127 +32,44 @@ internal class Program
         // connect too the orleans cluster
         builder.UseFx();
 
-        var MS_OIDC_SCHEME = "Oidc";
+        // BFF backend setup
+        builder.Services.AddBff();
 
-        // Add services to the container.
-        builder.Services.AddAuthentication(MS_OIDC_SCHEME)
-            .AddOpenIdConnect(MS_OIDC_SCHEME, oidcOptions =>
-            {
-                // For the following OIDC settings, any line that's commented out
-                // represents a DEFAULT setting. If you adopt the default, you can
-                // remove the line if you wish.
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = "cookie";
+        options.DefaultChallengeScheme = "oidc";
+        options.DefaultSignOutScheme = "oidc";
+    })
+    .AddCookie("cookie", options =>
+    {
+        options.Cookie.Name = "__Host-blazor";
+        options.Cookie.SameSite = SameSiteMode.Strict;
+    })
+    .AddOpenIdConnect("oidc", options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.Authority = Environment.GetEnvironmentVariable("KEYCLOAK_URL");
 
-                // ........................................................................
-                // The OIDC handler must use a sign-in scheme capable of persisting
-                // user credentials across requests.
+        options.ClientId = Environment.GetEnvironmentVariable("KEYCLOAK_CLIENT_ID");
+        options.ClientSecret = Environment.GetEnvironmentVariable("KEYCLOAK_CLIENT_SECRET");
 
-                oidcOptions.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                // ........................................................................
+        options.Authority.Should().NotBeNullOrEmpty("KEYCLOAK_URL is not set");
+        options.ClientId.Should().NotBeNullOrEmpty("KEYCLOAK_CLIENT_ID is not set");
+        options.ClientSecret.Should().NotBeNullOrEmpty("KEYCLOAK_CLIENT_SECRET is not set");
+        options.ResponseType = "code";
+        options.ResponseMode = "query";
 
-                // ........................................................................
-                // The "openid" and "profile" scopes are required for the OIDC handler
-                // and included by default. You should enable these scopes here if scopes
-                // are provided by "Authentication:Schemes:MicrosoftOidc:Scope"
-                // configuration because configuration may overwrite the scopes collection.
+        options.Scope.Clear();
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        options.Scope.Add("api");
+        options.Scope.Add("offline_access");
 
-                oidcOptions.Scope.Add(OpenIdConnectScope.OpenIdProfile);
-                // ........................................................................
-
-                // ........................................................................
-                // The following paths must match the redirect and post logout redirect
-                // paths configured when registering the application with the OIDC provider.
-                // The default values are "/signin-oidc" and "/signout-callback-oidc".
-
-                //oidcOptions.CallbackPath = new PathString("/signin-oidc");
-                //oidcOptions.SignedOutCallbackPath = new PathString("/signout-callback-oidc");
-                // ........................................................................
-
-                // ........................................................................
-                // The RemoteSignOutPath is the "Front-channel logout URL" for remote single
-                // sign-out. The default value is "/signout-oidc".
-
-                //oidcOptions.RemoteSignOutPath = new PathString("/signout-oidc");
-                // ........................................................................
-
-                // ........................................................................
-                // The following example Authority is configured for Microsoft Entra ID
-                // and a single-tenant application registration. Set the {TENANT ID}
-                // placeholder to the Tenant ID. The "common" Authority
-                // https://login.microsoftonline.com/common/v2.0/ should be used
-                // for multi-tenant apps. You can also use the "common" Authority for
-                // single-tenant apps, but it requires a custom IssuerValidator as shown
-                // in the comments below.
-
-                //oidcOptions.Authority = "https://login.microsoftonline.com/{TENANT ID}/v2.0/";
-                oidcOptions.RequireHttpsMetadata = false;
-                oidcOptions.Authority = Environment.GetEnvironmentVariable("KEYCLOAK_URL");
-                // ........................................................................
-
-                // ........................................................................
-                // Set the Client ID for the app. Set the {CLIENT ID} placeholder to
-                // the Client ID.
-
-                //oidcOptions.ClientId = "{CLIENT ID}";
-                oidcOptions.ClientId = Environment.GetEnvironmentVariable("KEYCLOAK_CLIENT_ID");
-                oidcOptions.ClientSecret = Environment.GetEnvironmentVariable("KEYCLOAK_CLIENT_SECRET");
-                // ........................................................................
-
-                // ........................................................................
-                // Setting ResponseType to "code" configures the OIDC handler to use
-                // authorization code flow. Implicit grants and hybrid flows are unnecessary
-                // in this mode. In a Microsoft Entra ID app registration, you don't need to
-                // select either box for the authorization endpoint to return access tokens
-                // or ID tokens. The OIDC handler automatically requests the appropriate
-                // tokens using the code returned from the authorization endpoint.
-
-                oidcOptions.ResponseType = OpenIdConnectResponseType.CodeIdTokenToken;
-                // ........................................................................
-
-                // ........................................................................
-                // Set MapInboundClaims to "false" to obtain the original claim types from
-                // the token. Many OIDC servers use "name" and "role"/"roles" rather than
-                // the SOAP/WS-Fed defaults in ClaimTypes. Adjust these values if your
-                // identity provider uses different claim types.
-
-                oidcOptions.MapInboundClaims = true;
-                oidcOptions.TokenValidationParameters.NameClaimType = "name";
-                oidcOptions.TokenValidationParameters.RoleClaimType = "roles";
-                // ........................................................................
-
-                // ........................................................................
-                // Many OIDC providers work with the default issuer validator, but the
-                // configuration must account for the issuer parameterized with "{TENANT ID}"
-                // returned by the "common" endpoint's /.well-known/openid-configuration
-                // For more information, see
-                // https://github.com/AzureAD/azure-activedirectory-identitymodel-extensions-for-dotnet/issues/1731
-
-                //var microsoftIssuerValidator = AadIssuerValidator.GetAadIssuerValidator(oidcOptions.Authority);
-                //oidcOptions.TokenValidationParameters.IssuerValidator = microsoftIssuerValidator.Validate;
-                // ........................................................................
-
-                // ........................................................................
-                // OIDC connect options set later via ConfigureCookieOidcRefresh
-                //
-                // (1) The "offline_access" scope is required for the refresh token.
-                //
-                // (2) SaveTokens is set to true, which saves the access and refresh tokens
-                // in the cookie, so the app can authenticate requests for weather data and
-                // use the refresh token to obtain a new access token on access token
-                // expiration.
-                // ........................................................................
-            })
-            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
-
-        // ConfigureCookieOidcRefresh attaches a cookie OnValidatePrincipal callback to get
-        // a new access token when the current one expires, and reissue a cookie with the
-        // new access token saved inside. If the refresh fails, the user will be signed
-        // out. OIDC connect options are set for saving tokens and the offline access
-        // scope.
-        builder.Services.ConfigureCookieOidcRefresh(CookieAuthenticationDefaults.AuthenticationScheme, MS_OIDC_SCHEME);
-
-        builder.Services.AddAuthorization();
-        builder.Services.AddAuthentication();
-        builder.Services.AddCascadingAuthenticationState();
+        options.MapInboundClaims = false;
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.SaveTokens = true;
+    });
 
 
         // Add Weather Service
@@ -184,9 +102,15 @@ internal class Program
 
         app.UseRouting();
         app.UseHttpsRedirection();
+
+        // BFF setup
         app.UseAuthentication();
+        app.UseBff();
         app.UseAuthorization();
+        app.MapBffManagementEndpoints();
         app.UseAntiforgery();
+
+
         app.MapStaticAssets();
         app.MapRazorComponents<App>()
             .AddInteractiveWebAssemblyRenderMode()
