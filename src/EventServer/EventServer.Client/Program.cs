da0a29d0c928e4a8dd178b7d9bf80e24f3227e37
Leo D'Angelo <ldangelo@mac.com>
2025-01-26 13:50:05 -0600

    Working BFF code
    The login/logout functionality is wonky
    but the rest works properly

--- src/EventServer/EventServer.Client/Program.cs
+++ src/EventServer/EventServer.Client/Program.cs
@@ -1,12 +1,19 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Blazor6.Client.BFF;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddMudServices();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, BffAuthenticationStateProvider>();
+// HTTP client configuration
+builder.Services.AddTransient<AntiforgeryHandler>();
 
-RegisterHttpClient(builder, builder.Services);
+builder.Services.AddHttpClient("backend", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<AntiforgeryHandler>();
+builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("backend"));
 
 var app = builder.Build();
 await app.RunAsync();
@@ -20,9 +27,19 @@ static void RegisterHttpClient(WebAssemblyHostBuilder builder, IServiceCollectio
             httpClientName,
             client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
         )
-        .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+        .AddHttpMessageHandler<AntiforgeryHandler>();
 
     services.AddScoped(sp =>
         sp.GetRequiredService<IHttpClientFactory>().CreateClient(httpClientName)
     );
+}
+
+public class AntiforgeryHandler : DelegatingHandler
+{
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        request.Headers.Add("X-CSRF", "1");
+        return base.SendAsync(request, cancellationToken);
+    }
 }
