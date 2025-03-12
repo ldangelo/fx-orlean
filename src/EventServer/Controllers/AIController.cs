9e17e9b3d9e98762893f8d1f6fb152cb5f6afe8c
Leo A. D'Angelo <ldangelo@mac.com>
2025-03-12 11:51:41 -0400

    Make problemdescription nullable

--- src/EventServer/Controllers/AIController.cs
+++ src/EventServer/Controllers/AIController.cs
@@ -1,21 +1,12 @@
-using EventServer.Aggregates.Calendar.Commands;
-using EventServer.Aggregates.Calendar.Events;
-using EventServer.Services;
 using Fortium.Types;
-using Google.Apis.Calendar.v3.Data;
-using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Serilog;
-using Wolverine.Http;
-using Wolverine.Marten;
-using static ChatGPTWithRAG;
-using Events = Google.Apis.Calendar.v3.Data.Events;
 
 namespace EventServer.Controllers;
 
 public class AIRequest
 {
-  public string ProblemDescription { get; set; }
+  public string? ProblemDescription { get; set; }
 }
 
 public class AIController : FastEndpoints.Endpoint<AIRequest, List<Partner>>
@@ -38,7 +29,7 @@ public class AIController : FastEndpoints.Endpoint<AIRequest, List<Partner>>
   {
     Log.Information("AIController: {description}", r.ProblemDescription);
 
-    var partners = _chatGPTWithRAG.GetChatGPTResponse(r.ProblemDescription).Result;
+    var partners = _chatGPTWithRAG.GetChatGPTResponse(r.ProblemDescription!).Result;
     Log.Information("AIController: {output}", JsonConvert.SerializeObject(partners));
     await SendAsync(partners);
   }
